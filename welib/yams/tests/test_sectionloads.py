d3e6f04c06d6a60b8a9587c43e1675bbc8d03121
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2021-05-25 16:27:00 -0600

    Fixed tests for python 2.6

--- welib/yams/tests/test_sectionloads.py
+++ welib/yams/tests/test_sectionloads.py
@@ -35,27 +35,27 @@ class Test(unittest.TestCase):
         Fs2, Ms2 = beamSectionLoads1D(z, p, F_top, M_top, s=1, F_lumped=F_lumped, method='manual')
         Fs3, Ms3 = beamSectionLoads1D(z, p, F_top, M_top, s=1, F_lumped=F_lumped, method='cumtrapz')
 
-        import matplotlib.pyplot as plt
-        fig,axes = plt.subplots(1, 2, sharey=True, figsize=(6.4,4.8)) # (6.4,4.8)
-        fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
+        #import matplotlib.pyplot as plt
+        #fig,axes = plt.subplots(1, 2, sharey=True, figsize=(6.4,4.8)) # (6.4,4.8)
+        #fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
 
-        axes[0].plot(Fs, z, label='Fs')
-        axes[0].plot(Fs2, z, '+', label='Fs (manual)')
-        axes[0].plot(Fs3, z, '.', label='Fs (cumtrapz)')
-        axes[0].plot([F_top,F_top], [z[0],z[-1]], 'k--')
-        axes[0].plot([F_tot,F_tot], [z[0],z[-1]], 'k--')
+        #axes[0].plot(Fs, z, label='Fs')
+        #axes[0].plot(Fs2, z, '+', label='Fs (manual)')
+        #axes[0].plot(Fs3, z, '.', label='Fs (cumtrapz)')
+        #axes[0].plot([F_top,F_top], [z[0],z[-1]], 'k--')
+        #axes[0].plot([F_tot,F_tot], [z[0],z[-1]], 'k--')
 
-        axes[1].plot(Ms, z, label='Ms')
-        axes[1].plot(Ms2, z, '+', label='Ms (manual)')
-        axes[1].plot(Ms3, z, '.', label='Ms (cumtrapz)')
-        axes[1].plot([M_top,M_top], [z[0],z[-1]], 'k--')
-        axes[1].plot([M_tot,M_tot], [z[0],z[-1]], 'k--')
-        
-        axes[0].set_ylabel('z [m]')
-        axes[0].set_xlabel('F [N]')
-        axes[1].set_xlabel('M [Nm]')
-        axes[0].legend()
-        axes[1].legend()
+        #axes[1].plot(Ms, z, label='Ms')
+        #axes[1].plot(Ms2, z, '+', label='Ms (manual)')
+        #axes[1].plot(Ms3, z, '.', label='Ms (cumtrapz)')
+        #axes[1].plot([M_top,M_top], [z[0],z[-1]], 'k--')
+        #axes[1].plot([M_tot,M_tot], [z[0],z[-1]], 'k--')
+        #
+        #axes[0].set_ylabel('z [m]')
+        #axes[0].set_xlabel('F [N]')
+        #axes[1].set_xlabel('M [Nm]')
+        #axes[0].legend()
+        #axes[1].legend()
 
         
         dz=np.diff(z)[0]
@@ -65,19 +65,19 @@ class Test(unittest.TestCase):
         p2   = np.interp(z2, z,p)
         Fs_2 = np.interp(z2, z,Fs)
 
-        fig,axes = plt.subplots(1, 2, sharey=True, figsize=(6.4,4.8)) # (6.4,4.8)
-        fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
+        #fig,axes = plt.subplots(1, 2, sharey=True, figsize=(6.4,4.8)) # (6.4,4.8)
+        #fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
 
-        axes[0].plot(p    , z ,'-', label='p')
-        axes[0].plot(p2   , z2,'-', label='p')
-        axes[0].plot(-dFsdz, z2,'--',     label='dFs/dz')
+        #axes[0].plot(p    , z ,'-', label='p')
+        #axes[0].plot(p2   , z2,'-', label='p')
+        #axes[0].plot(-dFsdz, z2,'--',     label='dFs/dz')
 
-        axes[1].plot(Fs   , z , '-', label='Fs')
-        axes[1].plot(-dMsdz, z2, '--',label='dMs/dz')
-        
-        axes[0].set_ylabel('z [m]')
-        axes[0].set_xlabel('p [N/m]')
-        axes[1].set_xlabel('F [N]')
+        #axes[1].plot(Fs   , z , '-', label='Fs')
+        #axes[1].plot(-dMsdz, z2, '--',label='dMs/dz')
+        #
+        #axes[0].set_ylabel('z [m]')
+        #axes[0].set_xlabel('p [N/m]')
+        #axes[1].set_xlabel('F [N]')
         #plt.show()
 
 
