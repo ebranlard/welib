6e87a4d8fa63976aab067387fb35ffbf14d53501
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2021-07-29 22:35:03 -0600

    Adding test for convolution

--- welib/tools/tests/test_signal.py
+++ welib/tools/tests/test_signal.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from welib.tools.signal import zero_crossings 
+from welib.tools.signal import convolution_integral 
 
 # --------------------------------------------------------------------------------}
 # ---  
@@ -22,6 +23,26 @@ class TestSignal(unittest.TestCase):
         self.assertEqual(zero_crossings(np.array([ 1,-1]),direction='up'  )[0].size,0)
         self.assertEqual(zero_crossings(np.array([-1, 1]),direction='down')[0].size,0)
         
+
+    def test_convolution(self):
+        # Test that convolution returns expected values using simple functions
+        time    = np.linspace(0, 2, 100)
+        f       = time
+        g       = time**2
+        fog_ref = time**4/12
+
+        fog = convolution_integral(time, f, g)
+
+        #import matplotlib.pyplot as plt
+        #fig,ax = plt.subplots(1, 1, sharey=False, figsize=(6.4,4.8)) # (6.4,4.8)
+        #fig.subplots_adjust(left=0.12, right=0.95, top=0.95, bottom=0.11, hspace=0.20, wspace=0.20)
+        #ax.plot(time, fog_ref, 'k-' , label='ref')
+        #ax.plot(time, fog    , '--' , label='num')
+        #ax.set_xlabel('time')
+        #ax.legend()
+        #plt.show()
+
+        np.testing.assert_almost_equal(fog, fog_ref, 3)
  
 if __name__ == '__main__':
     unittest.main()
