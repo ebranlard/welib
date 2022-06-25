72ecf9f5cc7076684eb0cb6ecc4ef766cd7118da
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2022-06-24 21:06:56 -0600

    Beams: adding more boundary conditions

--- welib/beams/theory.py
+++ welib/beams/theory.py
@@ -6,7 +6,9 @@ def UniformBeamBendingModes(Type,EI,rho,A,L,w=None,x=None,Mtop=0,norm='tip',nMod
     returns Mode shapes and frequencies for a uniform beam in bending
 
     References:
-      Inman : Engineering variation
+      [1] Inman : Engineering vibration
+      [2] Nielsen
+      [3] Thomsen: Vibration and stability
     
     Author: E. Branlard"""
     if x is None or len(x)==0:
@@ -14,35 +16,114 @@ def UniformBeamBendingModes(Type,EI,rho,A,L,w=None,x=None,Mtop=0,norm='tip',nMod
     if np.amax(x) != L:
         raise Exception('Max of x should be equal to L')
 
+    # Useful Intermediate functions [3] p374
+    F = lambda u: np.sinh(u) + np.sin(u)
+    G = lambda u: np.cosh(u) + np.cos(u)
+    H = lambda u: np.sinh(u) - np.sin(u)
+    J = lambda u: np.cosh(u) - np.cos(u)
+    Jp  = F
+    Jpp = G
+    Hp  = J
+    Hpp = F
+    Gp  = H
+    Gpp = J
+    Fp  = G
+    Fpp = H
+
+
     # Dimensionless spanwise position
     x0 = x / L
+    Type = Type.lower().replace('fixed','clamped')
     s = Type.split('-')
-    if s[0].lower()=='unloaded':
-        # --- "Theory" (clamped-free, vertical, no weight)
-        # See Inman, p.335 or Nielsen1 p129
-        if 'unloaded-clamped-free' == (Type.lower()):
+    if s[0] =='unloaded':
+
+        modesU = lambda x0, l: x0*0
+        modesV = lambda x0, l: x0*0
+        modesK = lambda x0, l: x0*0
+
+        # See [1] p.335  [2] p129 [3] p375
+        if   'unloaded-clamped-clamped' == Type:
+            freq_function = lambda x: -1 + np.cosh(x) * np.cos(x)
+            freq_guess    = lambda i: (2*(i+1)+1)*np.pi/2
+            modesU        = lambda x0, l: H(l) * J  (l*x0) - J(l) * H  (l*x0)
+            modesV        = lambda x0, l: H(l) * Jp (l*x0) - J(l) * Hp (l*x0)
+            modesK        = lambda x0, l: H(l) * Jpp(l*x0) - J(l) * Hpp(l*x0)
+        elif 'unloaded-clamped-hinged' == Type:
+            freq_function = lambda x: np.tan(x) - np.tanh(x)
+            freq_guess    = lambda i: (4*(i+1)+1)*np.pi/4
+            modesU        = lambda x0, l: H(l) * J  (l*x0) - J(l) * H  (l*x0)
+            modesV        = lambda x0, l: H(l) * Jp (l*x0) - J(l) * Hp (l*x0)
+            modesK        = lambda x0, l: H(l) * Jpp(l*x0) - J(l) * Hpp(l*x0)
+        elif 'unloaded-clamped-free' == Type:
             # NOTE: cosh(beta_n)cos(beta_n) =-1
             #  sigma_n = [ np.sinh(beta_n) - sin(beta_n) ]/[cosh(beta_n) + cos(beta_n)]
             #  for j>5, a good approx is B(j) = (2*j-1)np.pi/2  and S(j)=1;
             #B  = [1.87510407, 4.69409113, 7.85475744,10.99554073,14.13716839, (2*6-1)*np.pi/2];
             #S  = [0.734095514 1.018467319 0.999224497 1.000033553 0.999998550 1];
-            B = np.zeros(nModes)
-            for i in np.arange(nModes):
-                B[i] = sciopt.fsolve(lambda x: 1 + np.cosh(x) * np.cos(x), (2*(i+1)-1)*np.pi/2)
-        elif 'unloaded-topmass-clamped-free' == (Type.lower()):
+            freq_function = lambda x: 1 + np.cosh(x) * np.cos(x)
+            freq_guess    = lambda i: (2*(i+1)-1)*np.pi/2
+            modesU        = lambda x0, l: F(l) * J  (l*x0) - G(l) * H  (l*x0)
+            modesV        = lambda x0, l: F(l) * Jp (l*x0) - G(l) * Hp (l*x0)
+            modesK        = lambda x0, l: F(l) * Jpp(l*x0) - G(l) * Hpp(l*x0)
+        elif 'unloaded-clamped-guided' == Type:
+            freq_function = lambda x: np.tan(x) + np.tanh(x)
+            freq_guess    = lambda i: (4*(i+1)-1)*np.pi/4
+            modesU        = lambda x0, l: J(l) * J  (l*x0) - F(l) * H  (l*x0)
+            modesV        = lambda x0, l: J(l) * Jp (l*x0) - F(l) * Hp (l*x0)
+            modesK        = lambda x0, l: J(l) * Jpp(l*x0) - F(l) * Hpp(l*x0)
+        elif 'unloaded-topmass-clamped-free' == Type:
             # The geometrical stiffning is not accounted for here
             if Mtop is None:
                 raise Exception('Please specify value for Mtop for %s',Type)
             M = rho * A * L
-            B = np.zeros(nModes)
-            for i in np.arange(nModes):
-                B[i] = sciopt.fsolve(lambda x: 1+np.cosh(x)*np.cos(x)-x*Mtop/M*(np.sin(x)*np.cosh(x)-np.cos(x)*np.sinh(x)),(2*(i+1)-1)*np.pi/2)
+            freq_function = lambda x: 1+np.cosh(x)*np.cos(x)-x*Mtop/M*(np.sin(x)*np.cosh(x)-np.cos(x)*np.sinh(x))
+            freq_guess    = lambda i: (2*(i+1)-1)*np.pi/2
+            modesU        = lambda x0, l: F(l) * J  (l*x0) - G(l) * H  (l*x0)
+            modesV        = lambda x0, l: F(l) * Jp (l*x0) - G(l) * Hp (l*x0)
+            modesK        = lambda x0, l: F(l) * Jpp(l*x0) - G(l) * Hpp(l*x0)
+        elif 'unloaded-hinged-hinged' == Type: # simply-supported
+            freq_function = lambda x: np.sin(x) 
+            freq_guess    = lambda i: (i+1)*np.pi # NOTE: exact..
+            modesU        = lambda x0, l:  np.sin(l*x0)
+            modesV        = lambda x0, l:  np.cos(l*x0)
+            modesK        = lambda x0, l: -np.sin(l*x0)
+        elif 'unloaded-hinged-guided' == Type:
+            freq_function = lambda x: np.cos(x) 
+            freq_guess    = lambda i: (2*(i+1)-1)*np.pi/2 # NOTE: exact..
+            modesU        = lambda x0, l:  np.sin(l*x0)
+            modesV        = lambda x0, l:  np.cos(l*x0)
+            modesK        = lambda x0, l: -np.sin(l*x0)
+        elif 'unloaded-guided-guided' == Type:
+            freq_function = lambda x: np.sin(x) 
+            freq_guess    = lambda i: (i+1)*np.pi  # NOTE: exact..
+            modesU        = lambda x0, l:  np.cos(l*x0)
+            modesV        = lambda x0, l: -np.sin(l*x0)
+            modesK        = lambda x0, l: -np.cos(l*x0)
+        elif 'unloaded-free-free' == Type:
+            freq_function = lambda x: -1 + np.cosh(x) * np.cos(x)
+            freq_guess    = lambda i: (2*(i+1)+1)*np.pi/2
+            modesU        = lambda x0, l: H(l) * G  (l*x0) - J(l) * F  (l*x0)
+            modesV        = lambda x0, l: H(l) * Gp (l*x0) - J(l) * Fp (l*x0)
+            modesK        = lambda x0, l: H(l) * Gpp(l*x0) - J(l) * Fpp(l*x0)
+        elif 'unloaded-free-hinged' == Type:
+            freq_function = lambda x: np.tan(x) - np.tanh(x)
+            freq_guess    = lambda i: (4*(i+1)+1)*np.pi/4
+            modesU        = lambda x0, l: F(l) * G  (l*x0) - G(l) * F  (l*x0)
+            modesV        = lambda x0, l: F(l) * Gp (l*x0) - G(l) * Fp (l*x0)
+            modesK        = lambda x0, l: F(l) * Gpp(l*x0) - G(l) * Fpp(l*x0)
+        elif 'unloaded-free-guided' == Type:
+            freq_function = lambda x: np.tan(x) + np.tanh(x)
+            freq_guess    = lambda i: (4*(i+1)-1)*np.pi/4
+            modesU        = lambda x0, l: F(l) * G  (l*x0) - H(l) * F  (l*x0)
+            modesV        = lambda x0, l: F(l) * Gp (l*x0) - H(l) * Fp (l*x0)
+            modesK        = lambda x0, l: F(l) * Gpp(l*x0) - H(l) * Fpp(l*x0)
         else:
             raise Exception('unknown type %s',Type)
-        #S  = ( sinh(B)-sin(B) ) ./ ( cosh(B) + cos(B));  # Sigma
-        #C  = ( cosh(B)+cos(B) ) ./ ( sinh(B) + sin(B));  # Sigma
-        SS = np.sinh(B) + np.sin(B)
-        CC = np.cosh(B) + np.cos(B)
+
+        # Solve for "lambda"
+        B = np.zeros(nModes)
+        for i in np.arange(nModes):
+            B[i] = sciopt.fsolve(freq_function, freq_guess(i))
         # Frequency
         freq = (B / L) ** 2 / (2 * np.pi) * np.sqrt(EI / (rho * A))
         # --- Mode shapes
@@ -50,17 +131,13 @@ def UniformBeamBendingModes(Type,EI,rho,A,L,w=None,x=None,Mtop=0,norm='tip',nMod
         ModesV = np.zeros((len(B),len(x0)))
         ModesK = np.zeros((len(B),len(x0)))
         for i in np.arange(nModes):
-            ModesU[i,:] =            SS[i] * (np.cosh(B[i]*x0) - np.cos(B[i] * x0)) - CC[i] * (np.sinh(B[i] * x0) - np.sin(B[i] * x0))
-            ModesV[i,:] = B[i]    * (SS[i] * (np.sinh(B[i]*x0) + np.sin(B[i] * x0)) - CC[i] * (np.cosh(B[i] * x0) - np.cos(B[i] * x0)))
-            ModesK[i,:] = B[i]**2 * (SS[i] * (np.cosh(B[i]*x0) + np.cos(B[i] * x0)) - CC[i] * (np.sinh(B[i] * x0) + np.sin(B[i] * x0)))
-            #  ModesU(i,:)  =        cosh(B[i]*x0)-cos(B[i]*x0) - S[i]*(sinh(B[i]*x0)-sin(B[i]*x0)) ;
-            #  ModesV(i,:) = B[i]  *(sinh(B[i]*x0)+sin(B[i]*x0) - S[i]*(cosh(B[i]*x0)-cos(B[i]*x0)));
-            #  ModesK(i,:) = B[i]^2*(cosh(B[i]*x0)+cos(B[i]*x0) - S[i]*(sinh(B[i]*x0)+sin(B[i]*x0)));
-            #  ModesU(i,:)  =        cosh(B[i]*x0)-cos(B[i]*x0) - C[i]*(sinh(B[i]*x0)-sin(B[i]*x0)) ;
-            #  ModesV(i,:) = B[i]  *(sinh(B[i]*x0)+sin(B[i]*x0) - C[i]*(cosh(B[i]*x0)-cos(B[i]*x0)));
-            #  ModesK(i,:) = B[i]^2*(cosh(B[i]*x0)+cos(B[i]*x0) - C[i]*(sinh(B[i]*x0)+sin(B[i]*x0)));
-    elif s[0].lower()=='loaded':
-        if 'loaded-clamped-free' == (Type.lower()):
+            l = B[i]
+            ModesU[i,:] = modesU(x0,l)
+            ModesV[i,:] = modesV(x0,l) * l
+            ModesK[i,:] = modesK(x0,l) * l**2
+
+    elif s[0] == 'loaded':
+        if 'loaded-clamped-free' == Type:
             if w is None:
                 w = A * rho
             if L==0:
