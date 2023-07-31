0b9a6de9dd6bc959f1055451bc3ce72e9dc1d009
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-07-30 18:14:03 -0600

    Wind: using stoch for time series generation

--- welib/wind/spectra.py
+++ welib/wind/spectra.py
@@ -2,14 +2,16 @@ import numpy as np
 import pandas as pd
 
 
-def kaimal(f, V0, sigma, L):
+def kaimal(f, V0, sigma, L, angularFrequency=False):
     """ 
     Kaimal spectrum
 
     See also welib.standards.IEC for definition according to classes, ETM/NTM
 
     INPUTS:
-     - f: frequency (NOTE: can be in Hz or rad/s)
+     - f: frequency, in [Hz] if angularFrequency=False, in [rad/s] if angularFrequency is True
+             Sf(f) df = So(om)dom   =>   So(om) = Sf(f) / (2pi)
+
      - V0: reference/hub velocity velocity
      - sigma: wind standard deviation, turbulence intensity I= sigma/V0. 
      - L: turbulence length scale. 
@@ -17,7 +19,19 @@ def kaimal(f, V0, sigma, L):
     OUTPUTS:
       - S: kaimal spectrum
     """
-    return (4 * sigma**2 * L / V0) / (1 + 6 * f * L / V0) ** (5./3)
+    if angularFrequency:
+        # Input "f" is actually omega
+        omega = f
+        f = omega/(2*np.pi)
+        scale=1/(2*np.pi)
+    else:
+        scale=1
+    S = scale * (4 * sigma**2 * L / V0) / (1 + 6 * f * L / V0) ** (5./3) 
+    f = np.asarray(f)
+    S = np.asarray(S)
+    b=f==0
+    S[b] = 0
+    return S
 
 
 
